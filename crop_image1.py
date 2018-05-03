0b1c67c50381534f1a5f8229464cd8860738e105
ShivaNavya <navyashivas@gmail.com>
2018-05-03 11:47:00 +0530

    cropped image

--- crop_image1.py
+++ crop_image1.py
@@ -1,13 +1,9 @@
 from PIL import Image
 import os
-import numpy as np
-def get_cropped_image(image_path):
 
-    im = Image.open(image_path).convert('L')
-    cropped_img = im.crop((110,180,1000,600))
-    return np.array(cropped_img)
-
-
-if __name__ == "__main__":
-    new_image = get_cropped_image(os.path.abspath("car"))
-    print(new_image)
+im = Image.open(os.path.abspath("car")).convert('L')
+img = im.crop((100,100,950,600))
+img.show()
+print(img)
+im.show()
+print(im)
