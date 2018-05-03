5e6d4431dfa383b771a07dea9a788430715065d6
ShivaNavya <navyashivas@gmail.com>
2018-05-03 11:39:12 +0530

    define function
    to crop image

--- crop_image1.py
+++ crop_image1.py
@@ -1,8 +1,8 @@
 from PIL import Image
 import os
 import numpy as np
-
 def get_cropped_image(image_path):
+
     im = Image.open(image_path).convert('L')
     cropped_img = im.crop((110,180,1000,600))
     return np.array(cropped_img)
@@ -11,4 +11,3 @@ def get_cropped_image(image_path):
 if __name__ == "__main__":
     new_image = get_cropped_image(os.path.abspath("car"))
     print(new_image)
-
